758a1a70590cd0f072e4d119c789747ddb5432ab
Daniel Peterson <dpeterson@basecampcodingacademy.org>
2020-01-12 19:12:56 -0600

    Finished the main part of the projects.

--- handlebars-JS/data.js
+++ handlebars-JS/data.js
@@ -4,7 +4,7 @@ const PROJECTS = {
             projectLink: "https://github.com/Dannyp123/VFW-Post-4100",
             langs: "HTML/CSS, Python, Django, Postgresql, Materillized CSS, Google API",
             description: "This project is a webpage I got paid to do for the VFW Post 4100. I am hosting it on Heroku, but it is registered at www.vfwpost4100.com. It allows for a webmaster to login and add upcoming events and then delete them as the events happen. It also orders the events by most recent.",
-            img: "/images/vfwpageimg.png",
+            img: "images/vfwpageimg.png",
             pageLink: "http://www.vfwpost4100.com/"
         },
         {
@@ -14,7 +14,7 @@ const PROJECTS = {
             description: `
         This project is my Base Camp final. It is a newest version of my preexisting Dunn's Country Store page. I added a backend to allow people to reserve tablels in the store. I used Django's Registration and Login/logout features. I also used Send Grids API to send pregenerated emails to users, and Google Maps API to show a Google Maps view of the location.
         `,
-            img: "/images/table-reservation-project.png",
+            img: "images/table-reservation-project.png",
             pageLink: "https://dunns-country-store.herokuapp.com/"
         },
         {
@@ -24,7 +24,62 @@ const PROJECTS = {
             description: `
               This independent application is a Django, full stack application. It shows off cars for sale and the details for each. The goal of this application was to get more independent work under my belt, and I want to dedicate more time to development outside of school. 
               `,
-            img: "/images/Screenshot_2019-04-12 Buy-A-Ride.png"
-        }
+            img: "images/buy-a-ride.png"
+        },
+        {
+            title: "Dunn's Country Store",
+            projectLink: "https://github.com/Dannyp123/Dunns-Country-Store",
+            langs: "HTML, CSS, Bootstrap",
+            description: `This is a webpage that I would say is my first real webpage. I decided to see if I could
+              make a webpage for my place of work. The reason why is because I needed the practice, there
+              is not anything new online about the store I work at, and because I enjoy Front-end work.
+              Hopefully, I can get it pushed on a real domain name.`,
+            img: "images/dunn-orginal.png",
+            pageLink: "https://dannyp123.github.io/Dunns-Country-Store/"
+        },
+
+        {
+            title: "Tic-Tac-BOO",
+            projectLink: "https://github.com/Dannyp123/Tic-Tac-BOO",
+            langs: "HTML, CSS, JavaScript",
+            description: `This is a JavaScript program that I made that is supposed to be the game Tic-Tac-Toe. I made it a
+          Halloween theme since when I made it, it was close to halloween. I used a spooky background image
+          and unique game pieces. Using HTML
+          and CSS to make it online and look good.`,
+            img: "images/halloween-project.jpg",
+            pageLink: "https://dannyp123.github.io/Tic-Tac-BOO/"
+        },
+        {
+            title: "Terminal Based Python Rental Store",
+            projectLink: "https://github.com/Dannyp123/rental_store_danielpeterson",
+            langs: " Python, Terminal",
+            description: `        This project is a Python program that I made in my coding class, which uses Visual Studio Code, for
+                  the final project
+                  in
+                  Python. It simulates tool rental
+                  store. It prompts the user to choose a tool and calculates the total tax plus a 10%
+                  deposit
+                  for
+                  a rental fee.`,
+            videoLink: "https://asciinema.org/a/XVRI0ApfqB2HUdQDko7Rsg2cI",
+            videoImg: "https://asciinema.org/a/XVRI0ApfqB2HUdQDko7Rsg2cI.svg"
+        },
+        {
+            title: "Guessing-game",
+            projectLink: "https://github.com/Dannyp123/Guessing-game",
+            langs: "Python, Terminal",
+            description: `        This project is a Python program that I wrote at home using Visual Studio Code. I watched a video
+                  and saw a very
+                  basic version of this program, so
+                  I decided to recreate one, but with more complexity. To use the program, it ask you for
+                  a
+                  number
+                  that is in between a given range of numbers, and tells you if you need to guess higher
+                  or
+                  lower
+                  and quits if you get the correct number.`,
+            videoLink: "https://asciinema.org/a/IOFeivcRaH2c4QFkWe8Lc1VJe",
+            videoImg: "https://asciinema.org/a/IOFeivcRaH2c4QFkWe8Lc1VJe.svg"
+        },
     ]
 }
